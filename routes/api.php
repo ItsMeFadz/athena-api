3161c2c9988f3cb63e262456e95f726852e226b6
ItsMeFadz <mr954904@gmail.com>
2026-09-02 15:31:20 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,5 @@ use App\Http\Controllers\Api\LunasKreditSyncController;
 use App\Http\Controllers\Api\TagihanKreditSyncController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('sync/lunas-kredit')->group(function ()
-{
-    Route::post('send', [LunasKreditSyncController::class, 'send']);
-    Route::post('receive', [LunasKreditSyncController::class, 'receive']);
-});
-
-Route::prefix('sync/tagihan-kredit')->group(function ()
-{
-    Route::post('receive', [TagihanKreditSyncController::class, 'receive']);
-});
+Route::post('sync/lunas-kredit/receive', [LunasKreditSyncController::class, 'receive']);
+Route::post('sync/tagihan-kredit/receive', [TagihanKreditSyncController::class, 'receive']);
